852b873629e6c7a89ca89a6d07ef658faa07a775
Salgado3 <jsalg10@gmail.com>
2026-01-02 10:25:46 -0600

    update navbar links. smooth scroll. add screen shots, faq, how to. wip.

---
+++ javascript/index.js
@@ -0,0 +1,10 @@
+const faqNavbar = document.getElementById("FAQ_navbar");
+const faqTitle = document.getElementById("FAQ");
+
+// faqNavbar.addEventListener("click", () => {
+//   faqTitle.scrollIntoView({
+//     behavior: "smooth",
+//     block: "end",
+//     inline: "nearest",
+//   });
+// });
